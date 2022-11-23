334cadbddb3031965b0effaafa7e9f2778f08311
Swapg1711 <swapnilsgathibandhe@gmail.com>
2022-11-23 20:17:25 +0530

    model 2 is the final model

--- model.py
+++ model.py
@@ -1,3 +1,14 @@
 
 
 print("Adiing all ML related code here")
+
+def model1():
+    print("Adding code for ML model 1")
+
+def model2():
+    print("Adding code for ML model 1")
+
+def model3():
+    print("Adding code for ML model 1")
+
+print("Model 2 is performing best")
